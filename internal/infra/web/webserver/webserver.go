d952d981a412ae70459dd10315829e10202303a3
René Bizelli <renebizelli@gmail.com>
2025-01-30 23:14:04 -0300

    api ok

--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,29 +10,47 @@ import (
 
 type WebServer struct {
 	Router        chi.Router
-	Handlers      map[string]http.HandlerFunc
+	Handlers      map[string]map[string]http.HandlerFunc
 	WebServerPort string
 }
 
 func NewWebServer(serverPort string) *WebServer {
+	h := make(map[string]map[string]http.HandlerFunc)
+	h["GET"] = make(map[string]http.HandlerFunc)
+	h["POST"] = make(map[string]http.HandlerFunc)
+
 	return &WebServer{
 		Router:        chi.NewRouter(),
-		Handlers:      make(map[string]http.HandlerFunc),
+		Handlers:      h,
 		WebServerPort: serverPort,
 	}
 }
 
-func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
-	s.Handlers[path] = handler
+func (s *WebServer) AddGetHandler(path string, handler http.HandlerFunc) {
+	s.addHandler("GET", path, handler)
+}
+
+func (s *WebServer) AddPostHandler(path string, handler http.HandlerFunc) {
+	s.addHandler("POST", path, handler)
+}
+
+func (s *WebServer) addHandler(method string, path string, handler http.HandlerFunc) {
+	fmt.Println("Adding handler", method, path)
+	s.Handlers[method][path] = handler
 }
 
 // loop through the handlers and add them to the router
 // register middeleware logger
 // start the server
 func (s *WebServer) Start() {
+
 	s.Router.Use(middleware.Logger)
-	for path, handler := range s.Handlers {
-		s.Router.Handle(path, handler)
+
+	for method, _map := range s.Handlers {
+		for path, handler := range _map {
+			s.Router.Method(method, path, handler)
+		}
 	}
+
 	http.ListenAndServe(s.WebServerPort, s.Router)
 }
